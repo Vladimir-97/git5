061ce64843923aa3ec9173eb98e310e24934522c
Vladimir-97 <78786084+Vladimir-97@users.noreply.github.com>
2021-03-20 09:57:39 +0500

    I'm create new function

--- git5/main.cpp
+++ git5/main.cpp
@@ -2,10 +2,19 @@
 
 using namespace std;
 
+
+int Hello_from_vova() {
+
+	cout << "Hello this is function create by Vova.";
+
+	return 0;
+}
+
+
 int main() {
 	cout << "Hello from Timur!!!";
 	cout << "Hello world!!!";
 	cout << "Hello from Vova!!!";
-
+	Hello_from_vova();
 	return 0;
 }
